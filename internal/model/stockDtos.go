1cc4aa7abc8873a8541118ccdefab293ab5a8295
Steponas Kaminskas <132608893+SteponasK@users.noreply.github.com>
2026-03-29 20:41:15 +0300

    stock dtos and repository

---
+++ internal/model/stockDtos.go
@@ -0,0 +1,29 @@
+package model
+
+type StockQuote struct {
+	Symbol    string  `json:"symbol"`
+	Price     float64 `json:"price"`
+	Volume    float64 `json:"volume"`
+	Timestamp int64   `json:"timestamp"`
+}
+
+type StockSearchResult struct {
+	Symbol      string `json:"symbol"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+}
+
+type StockProfile struct {
+	Symbol    string  `json:"symbol"`
+	Name      string  `json:"name"`
+	Logo      string  `json:"logo"`
+	Industry  string  `json:"industry"`
+	Country   string  `json:"country"`
+	Exchange  string  `json:"exchange"`
+	MarketCap float64 `json:"marketCap"`
+	WebURL    string  `json:"webUrl"`
+}
+
+type QuotesRequest struct {
+	Symbols []string `json:"symbols" binding:"required"`
+}
