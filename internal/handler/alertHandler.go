bb041881b758b0f6d489842917aa915e8f500872
Steponas Kaminskas <132608893+SteponasK@users.noreply.github.com>
2026-05-04 00:26:53 +0300

    STC-4 implement sprint backend features

---
+++ internal/handler/alertHandler.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/TradeLayers/BE/internal/appErrors"
+	"github.com/TradeLayers/BE/internal/model"
+	"github.com/TradeLayers/BE/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type AlertHandler struct {
+	service service.AlertService
+}
+
+func NewAlertHandler(s service.AlertService) *AlertHandler {
+	return &AlertHandler{service: s}
+}
+
+func (h *AlertHandler) List(c *gin.Context) {
+	userCtx := getUserContext(c)
+
+	alerts, err := h.service.List(*userCtx)
+	if err != appErrors.ErrNone {
+		appErrors.ReturnError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, alerts)
+}
+
+func (h *AlertHandler) Create(c *gin.Context) {
+	userCtx := getUserContext(c)
+
+	var req model.AlertRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		appErrors.ReturnError(c, appErrors.ErrInvalidFieldInformation)
+		return
+	}
+
+	alert, domainErr := h.service.Create(*userCtx, req)
+	if domainErr != appErrors.ErrNone {
+		appErrors.ReturnError(c, domainErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, alert)
+}
+
+func (h *AlertHandler) Delete(c *gin.Context) {
+	userCtx := getUserContext(c)
+
+	err := h.service.Delete(*userCtx, c.Param("id"))
+	if err != appErrors.ErrNone {
+		appErrors.ReturnError(c, err)
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
